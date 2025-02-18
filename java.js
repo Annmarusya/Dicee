8b8f9ab04e91147a2fd87e0b50a75fd28096d90c
Annmarusya <107357737+Annmarusya@users.noreply.github.com>
2025-02-18 19:45:09 +0200

    Add files via upload

--- java.js
+++ java.js
@@ -1,21 +1,18 @@
 
-var randomNumber1 = Math.floor(Math.random() * 6 + 1);
-var source1 = "./images/"+ randomNumber1 + ".png";
-document.querySelector(".img1").setAttribute("src", source1)
+    document.querySelector(".roll-button").addEventListener("click", function() {
+        var randomNumber1 = Math.floor(Math.random() * 6 + 1);
+        var source1 = "./images/" + randomNumber1 + ".png";
+        document.querySelector(".img1").setAttribute("src", source1);
 
+        var randomNumber2 = Math.floor(Math.random() * 6 + 1);
+        var source2 = "./images/" + randomNumber2 + ".png";
+        document.querySelector(".img2").setAttribute("src", source2);
 
-var randomNumber2 = Math.floor(Math.random() * 6 + 1);
-var source2 = "./images/"+ randomNumber2 + ".png";
-document.querySelector(".img2").setAttribute("src", source2)
-
-if (randomNumber1>randomNumber2){
-document.querySelector("h1").innerHTML = "<img src='./images/heart-svgrepo-com.svg'> Player 1 won"
-}
-
-if (randomNumber2>randomNumber1){
-document.querySelector("h1").innerHTML = " Player 2 won<img src='./images/heart-svgrepo-com.svg'>"
-}
-
-if (randomNumber2===randomNumber1){
-    document.querySelector("h1").innerHTML = "Draw!"
-}
+        if (randomNumber1 > randomNumber2) {
+            document.querySelector("h1").innerHTML = "<img src='./images/heart-svgrepo-com.svg'> Player 1 wins";
+        } else if (randomNumber2 > randomNumber1) {
+            document.querySelector("h1").innerHTML = "Player 2 wins <img src='./images/heart-svgrepo-com.svg'>";
+        } else {
+            document.querySelector("h1").innerHTML = "Draw!";
+        }
+    });
